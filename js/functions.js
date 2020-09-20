3e845af4a0a6d3d8e8afd4959b055e95f2004a59
lear <acramirez38@gmail.com>
2020-09-20 16:07:22 -0500

    Lightbox integration

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,14 @@
 // Functions for jQuery Lightbox
+
+jQuery(document).ready(function ($) {
+
+	'use strict';
+    
+  console.log( "cowabunga!" );
+
+  // Light Gallery
+  $(".lightbox-gallery").lightGallery({
+    selector: '.image'
+  });
+
+});
