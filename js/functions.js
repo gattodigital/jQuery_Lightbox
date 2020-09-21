d1385a0f20751a9929a60cfe04fffdd8ff02dfcb
lear <acramirez38@gmail.com>
2020-09-20 22:49:42 -0500

    style updates

--- js/functions.js
+++ js/functions.js
@@ -11,4 +11,21 @@ jQuery(document).ready(function ($) {
     selector: '.image'
   });
 
+  // Percentage Scroller
+  $(window).scroll(function(e){
+    var scrollTop = $(window).scrollTop();
+    var docHeight = $(document).height();
+    var winHeight = $(window).height();
+    var scrollPercent = (scrollTop) / (docHeight - winHeight);
+    var percentScroller = Math.round(scrollPercent*100);
+      $('#percentScroller>span').html(percentScroller);
+      repositionLabel();
+  });
+  $('#percentScroller').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 1000);
+    return false;
+  });
+
 });
